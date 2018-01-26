b77416f8dc052beca9ed4b3772d990de60ae3d23
anuu11 <35534904+anuu11@users.noreply.github.com>
2018-01-26 11:21:42 +0530

    Create anu38.c

---
+++ anu38.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+	int  i,k;
+    printf("enter two  numbers");
+    scanf("%d%d",&i,&k);
+    printf(" value of i=%d k=%d before swapping", i, k);
+
+    i = i ^ k;
+    k = i ^ k;
+    i = i ^ k;
+    printf("value of i=%d k=%d after swapping", i, k);
+
+    return 0;
+}
